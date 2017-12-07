0a7ff652d4b227c6f255781aba10b7aa6c05a776
Satyanarayana Reddy Konda Reddy <34239615+im-kondareddy@users.noreply.github.com>
2017-12-07 22:09:25 +0530

    Add files via upload

---
+++ com/sms_account.py
@@ -0,0 +1,44 @@
+import requests
+
+class SmsAccount():
+  main_auth_id = 'xxxxxxxxxxxxxxxxxxxx'
+  main_auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  sub_auth_id = 'xxxxxxxxxxxxxxxxxxxx'
+  sub_auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  url = 'https://api.plivo.com/v1/Account/'
+
+
+  """ For retrieving the data related to main account """
+  
+  def get_account_details(self):
+    response = requests.get(self.url+self.main_auth_id, auth = (self.main_auth_id, self.main_auth_token))
+    return(response.json())
+
+
+  """ For retrieving the data related to Sub account """
+
+  def get_subaccount_details(self):
+    response = requests.get(self.url+self.main_auth_id+'/Subaccount/'+self.sub_auth_id, auth = (self.main_auth_id, self.main_auth_token))
+    return(response.json())
+
+
+  """ For sending message from souce number to destination """
+
+  def sms_send(self, fromMobile, toMobile, message):
+    src, dst, text = fromMobile, toMobile, message
+    response = requests.post(self.url+self.main_auth_id+'/Message/', auth = (self.main_auth_id, self.main_auth_token), json = {'src':src, 'dst':dst, 'text':text})
+    return(response.json())
+
+
+  """ For getting the price chart """
+
+  def sms_price(self, country):
+     response = requests.get(self.url+self.main_auth_id+'/Pricing/', auth = (self.main_auth_id, self.main_auth_token), params = {'country_iso' : country} )
+     return (response.json())
+
+
+  """ For getting message details """
+
+  def sms_details(self, uuid):
+     response = requests.get(self.url+self.main_auth_id+'/Message/'+uuid, auth = (self.main_auth_id, self.main_auth_token))
+     return (response.json())
